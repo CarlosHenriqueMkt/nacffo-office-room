7cabb15d861f3a3c32764e3c5ae4f52ea6ac60a0
carloshenriquemkt <falacom.ocarlos@gmail.com>
2024-07-02 14:31:35 -0300

    vite config 3

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  optimizeDeps: {
-    include: ['three'], // Ensure 'three' is included in optimizeDeps
-  },
-  resolve: {
-    extensions: ['.js', '.glb'], // Include .glb in extensions if necessary
-  },
+  plugins: [glsl()],
+  assetsInclude: ["**/*.glb"],
 });
